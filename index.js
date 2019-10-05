520d30ff39e063432ad9d746a82918cfee53b01c
jarupong pajakgo <lagmanmc@gmail.com>
2019-10-05 22:50:48 +0700

    init

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
+console.log("Public Path: ",__dirname);
+app.use(function (req, res, next) {
+    var filename = path.basename(req.url);
+    console.log(`Fuzzing path: ${__dirname}/${filename}`);
+    next();
+});
+
 app.use(express.static(__dirname));
-console.log(__dirname);
 app.get('/',(req,res)=>{
     res.send("Hello");
 })
