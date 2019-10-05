da64d38bd34abca2f7d9591cbbc40fda39cd190a
jarupong pajakgo <lagmanmc@gmail.com>
2019-10-05 22:46:15 +0700

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname));
+console.log(__dirname);
+app.get('/',(req,res)=>{
+    res.send("Hello");
+})
+app.listen(3000)
